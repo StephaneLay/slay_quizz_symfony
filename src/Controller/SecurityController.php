25c29e19e40e74cad9fcadb2fde156b78d274592
StephaneLay <stephane.lay@hotmail.fr>
2025-07-16 14:45:12 +0200

    sign-in+login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,14 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\SubscriptionFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/login', name: 'app_login')]
+    #[Route(path: '/connect/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // get the login error if there is one
@@ -24,9 +31,45 @@ class SecurityController extends AbstractController
         ]);
     }
 
-    #[Route(path: '/logout', name: 'app_logout')]
+    #[Route(path: '/connect/logout', name: 'app_logout')]
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route('/connect/sign-in', name: 'signin')]
+    public function Signin(
+        Request $request,
+        EntityManagerInterface $em,
+        UserPasswordHasherInterface $hasher,
+        HttpClientInterface $spamChecker
+    ): Response {
+
+        $user = new User();
+        $signForm = $this->createForm(SubscriptionFormType::class, $user);
+        $signForm->handleRequest($request);
+
+
+        if ($signForm->isSubmitted() && $signForm->isValid()) {
+
+
+            $hashedPassword = $hasher->hashPassword(
+                $user,
+                $user->getPassword()
+            );
+            $user->setPassword($hashedPassword);
+            $user->setRoles([$signForm->get('role')->getData()]);
+
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('security/signin.html.twig', [
+            'controller_name' => 'HomeController',
+            'form' => $signForm
+
+        ]);
     }
 }
---
+++ src/Form/SubscriptionFormType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SubscriptionFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class)
+            ->add('name',TextType::class)
+            ->add('password', PasswordType::class)
+            ->add('role', ChoiceType::class, [
+                'choices' => [
+                    
+                    'admin' => 'ROLE_ADMIN',
+                    'Normal' => 'ROLE_USER'
+                ],
+                'mapped' => false
+            ], )
+            ->add("Subscribe", SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
